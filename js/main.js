b69d3c88e938fe26b8e9acf5559d46e90b2f6a99
milan978gh <amilandev78@gmail.com>
2021-01-29 16:33:43 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/*
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1
+  },
+  draw: function() {
+    console.log('draw');
+  }
+};
+circle.draw();
+*/
+/*
+// factory function
+function createCircle(radius) {
+  return {
+    //ES5
+    // radius:radius,
+    // ES6
+    radius,
+    draw: function() {
+      console.log('draw');
+    }
+  };
+}
+const circle = createCircle(1);
+circle.draw();
+*/
+/*
+// constructor function
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log('draw');
+  };
+}
+const circle = new Circle(1);
+*/
+
+
