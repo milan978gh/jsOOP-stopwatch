4885dd281403b54193fe8d4d7eeeb59463349f2f
milan978gh <amilandev78@gmail.com>
2021-02-01 12:33:57 +0100

    update,has glitch

--- js/main.js
+++ js/main.js
@@ -38,4 +38,34 @@ function Circle(radius) {
 const circle = new Circle(1);
 */
 
+const count = document.querySelector('.count');
+const btns = document.querySelectorAll('.btn');
+let duration = 0;
+let startTime, stopTime = 0;
 
+btns.forEach((btn) => {
+  btn.onclick = () => {
+    const swState = btn.classList.contains('start') ? start() :
+      btn.classList.contains('stop') ? stop() :
+    reset();
+    count.textContent = swState;
+  };
+});
+
+function start() {
+  startTime = new Date();
+  stopTime = 0;
+  return duration;
+}
+function stop() {
+  stopTime = new Date();
+  const seconds = (stopTime.getTime() - startTime.getTime()) / 1000;
+  duration += seconds;
+  return duration;
+}
+function reset() {
+  startTime = 0;
+  stopTime = 0;
+  duration = 0;
+  return duration;
+}
